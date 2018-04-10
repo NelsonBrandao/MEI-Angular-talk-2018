d6ebb1cd961a7964810ca0e2f0fc3f4bfeb8df30
Nelson Brandão <nelsonbrandao800@gmail.com>
2018-04-10 14:11:53 +0100

    Reseted back to initial stage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,39 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes, Router } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { CurrencyCardComponent } from '../components/currency-card/currency-card.component';
-import { ListCurrenciesPage } from '../pages/list-currencies.page';
-
-import { CoinMarketCapService } from '../services/CoinMarketCap';
-
-const appRoutes: Routes = [
-  { path: '', component: ListCurrenciesPage },
-  { path: '**', pathMatch: 'full', redirectTo: '/' },
-];
 
 @NgModule({
   declarations: [
     AppComponent,
-
-    CurrencyCardComponent,
-
-    ListCurrenciesPage,
   ],
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes),
   ],
-  entryComponents: [
-    ListCurrenciesPage,
-  ],
-  providers: [
-    CoinMarketCapService,
-  ],
+  entryComponents: [],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
